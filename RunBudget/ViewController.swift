9a2aea0aa8abc0c609e244ecd7bc757bf6da1682
Daniel Sauble <djsauble@gmail.com>
2016-12-18 22:10:49 -0800

    Add simple bar visualizations for the run budget/weekly goal categories. Change precision to tenths of a mile.

--- RunBudget/ViewController.swift
+++ RunBudget/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var thisWeekLabel: UILabel!
     @IBOutlet weak var unitControl: UISegmentedControl!
     @IBOutlet weak var trendControl: TrendControl!
+    @IBOutlet weak var thisWeekControl: WeekControl!
+    @IBOutlet weak var runBudgetControl: WeekControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -72,14 +74,20 @@ class ViewController: UIViewController {
             if let point = point {
                 if UnitStore.shared.unit == HKUnit.mile() {
                     self.howFarLabel.text = "\(point.rightNow) miles"
-                    self.thisWeekLabel.text = "\(Int(point.thisWeek)) of \(Int(point.targetMileage)) miles"
+                    self.thisWeekLabel.text = "\(Double(Int(point.thisWeek * 10.0)) / 10.0) of \(Double(Int(point.targetMileage * 10.0)) / 10.0) miles"
                     self.lastWeekLabel.text = "\(Int(point.lastWeek)) miles"
                 }
                 else {
-                    self.howFarLabel.text = "\(Int(point.rightNow / 1000)) km"
-                    self.thisWeekLabel.text = "\(Int(point.thisWeek / 1000)) of \(Int(point.targetMileage / 1000)) km"
-                    self.lastWeekLabel.text = "\(Int(point.lastWeek / 1000)) km"
+                    self.howFarLabel.text = "\(Double(Int(point.rightNow / 1000 * 10)) / 10.0) km"
+                    self.thisWeekLabel.text = "\(Double(Int(point.thisWeek / 1000 * 10)) / 10.0) of \(Double(Int(point.targetMileage / 1000 * 10)) / 10.0) km"
+                    self.lastWeekLabel.text = "\(Double(Int(point.lastWeek / 1000 * 10)) / 10.0) km"
                 }
+                
+                self.thisWeekControl.total = point.targetMileage
+                self.thisWeekControl.soFar = point.thisWeek
+                
+                self.runBudgetControl.total = point.targetMileage
+                self.runBudgetControl.soFar = point.thisWeek + Double(point.rightNow)
             }
             else {
                 if UnitStore.shared.unit == HKUnit.mile() {
---
+++ RunBudget/WeekControl.swift
@@ -0,0 +1,125 @@
+//
+//  WeekControl.swift
+//  RunBudget
+//
+//  Created by Daniel Sauble on 12/18/16.
+//  Copyright © 2016 Daniel Sauble. All rights reserved.
+//
+
+import UIKit
+
+class WeekControl: UIView {
+    
+    // MARK: Properties
+    
+    var soFar: Double = 0.0 {
+        didSet {
+            render()
+        }
+    }
+    var total: Double = 0.0 {
+        didSet {
+            render()
+        }
+    }
+    var soFarView: UIView? = nil
+    var remainingView: UIView? = nil
+    
+    // MARK: Initialization
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        
+        var progress = self.soFar
+        var remaining = self.total - self.soFar
+        let total = self.total
+        
+        if self.soFar > self.total {
+            progress = self.total
+            remaining = 0.0
+        }
+        
+        // Calculate bar dimensions
+        let spacing = 1
+        var bar1Width = CGFloat(0.0)
+        var bar2Width = CGFloat(0.0)
+        let barHeight = CGFloat(Double(frame.size.height))
+        
+        if progress == self.total {
+            bar1Width = frame.size.width
+            bar2Width = 0.0
+        }
+        else {
+            bar1Width = (frame.size.width * CGFloat(progress / total)) - CGFloat(spacing)
+            bar2Width = (frame.size.width * CGFloat(remaining / total))
+        }
+        
+        // Layout the bars
+        if let bar = self.soFarView {
+            let barFrame = CGRect(x: CGFloat(0.0), y: CGFloat(0.0), width: bar1Width, height: barHeight)
+            bar.frame = barFrame
+        }
+        if let bar = self.remainingView {
+            if progress < self.total {
+                let barFrame = CGRect(x: CGFloat(bar1Width) + CGFloat(spacing), y: CGFloat(0.0), width: bar2Width, height: barHeight)
+                bar.frame = barFrame
+            }
+        }
+    }
+
+    func render() {
+        
+        var progress = self.soFar
+        var remaining = self.total - self.soFar
+        let total = self.total
+        
+        if self.soFar > self.total {
+            progress = self.total
+            remaining = 0.0
+        }
+        
+        // Remove the existing bars
+        if let bar = self.soFarView {
+            bar.removeFromSuperview()
+        }
+        if let bar = self.remainingView {
+            bar.removeFromSuperview()
+        }
+        
+        // Calculate bar dimensions
+        let spacing = 1
+        var bar1Width = CGFloat(0.0)
+        var bar2Width = CGFloat(0.0)
+        let barHeight = CGFloat(Double(frame.size.height))
+
+        if progress == self.total {
+            bar1Width = frame.size.width
+            bar2Width = 0.0
+        }
+        else {
+            bar1Width = (frame.size.width * CGFloat(progress / total)) - CGFloat(spacing)
+            bar2Width = (frame.size.width * CGFloat(remaining / total))
+        }
+
+        // Add the new bars
+        let soFarView: UIView? = UIView(frame: CGRect(x: 0, y: 0, width: bar1Width, height: barHeight))
+        if let bar = soFarView {
+            bar.backgroundColor = UIColor.blue
+            self.soFarView = bar
+            self.addSubview(bar)
+        }
+        
+        let remainingView: UIView? = UIView(frame: CGRect(x: 0, y: 0, width: bar2Width, height: barHeight))
+        if let bar = remainingView {
+            bar.backgroundColor = UIColor.gray
+            self.remainingView = bar
+            self.addSubview(bar)
+        }
+    
+        // Perform layout
+        self.setNeedsLayout()
+    }
+}
