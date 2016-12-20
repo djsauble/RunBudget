5998ea0158648652e4c07e10007ffea31443bcd4
Daniel Sauble <djsauble@gmail.com>
2016-12-20 12:06:53 -0800

    Add logic to display remaining budget and save workout when complete

--- RunBudget WatchKit Extension/RunController.swift
+++ RunBudget WatchKit Extension/RunController.swift
@@ -14,7 +14,9 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
 
     var workoutSession: HKWorkoutSession? = nil
     var healthStore: HKHealthStore? = nil
+    var configuration: HKWorkoutConfiguration? = nil
     var runBudget: Int? = nil
+    var unit: HKUnit? = nil
     var distance: Double = 0
     
     @IBOutlet var runBudgetLabel: WKInterfaceLabel!
@@ -25,13 +27,14 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
         // Configure interface objects here.
         loadContext(context: context)
         
-        // Update the interface
-        updateInterface()
+        // Set the units
+        self.unit = UnitStore.shared.unit
         
         // Start a run
         let configuration = HKWorkoutConfiguration()
         configuration.activityType = .running
         configuration.locationType = .outdoor
+        self.configuration = configuration
         
         do {
             let workoutSession = try HKWorkoutSession(configuration: configuration)
@@ -51,6 +54,9 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
             // Perform proper error handling here
             fatalError("*** Unable to create the workout session: \(error.localizedDescription) ***")
         }
+        
+        // Update the interface
+        updateInterface()
     }
     
     override func willActivate() {
@@ -79,11 +85,11 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
     
     func updateInterface() {
         if let runBudget = self.runBudget {
-            if UnitStore.shared.unit == HKUnit.mile() {
-                runBudgetLabel.setText("\(runBudget) miles")
+            if self.unit == HKUnit.mile() {
+                runBudgetLabel.setText("\(Double(Int((Double(runBudget) - self.distance) * 100.0)) / 100.0) mi left")
             }
             else {
-                runBudgetLabel.setText("\(runBudget) km")
+                runBudgetLabel.setText("\(Double(Int((Double(runBudget) - self.distance) * 100.0)) / 100.0) km left")
             }
         }
     }
@@ -99,7 +105,7 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
         }
         else if toState == .ended {
             if fromState == .running {
-                // Save workout
+                saveSamples()
             }
         }
     }
@@ -109,7 +115,6 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
     }
     
     func startSampling() {
-        print("Start sampling...")
         if let workoutSession = self.workoutSession, let healthStore = self.healthStore {
             // Object predicate
             
@@ -127,14 +132,16 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
             let updateHandler: (HKAnchoredObjectQuery, [HKSample]?, [HKDeletedObject]?, HKQueryAnchor?, Error?) -> Void = {
                 query, samples, deletedObjects, queryAnchor, error in
 
+                // Aggregate samples
                 if let samples = samples as? [HKQuantitySample] {
-
                     for sample in samples {
-                        self.distance += sample.quantity.doubleValue(for: UnitStore.shared.unit)
+                        self.distance += sample.quantity.doubleValue(for: self.unit ?? HKUnit.mile())
                     }
-                    
-                    print("\(self.distance) miles")
                 }
+                print(self.distance)
+                
+                // Update the interface
+                self.updateInterface()
             }
             
             let query = HKAnchoredObjectQuery(type: quantityType!,
@@ -146,6 +153,37 @@ class RunController: WKInterfaceController, HKWorkoutSessionDelegate {
             query.updateHandler = updateHandler
             
             healthStore.execute(query)
+        }
+    }
+    
+    func saveSamples() {
+        if let workoutSession = self.workoutSession, let healthStore = self.healthStore {
+            let startDate = workoutSession.startDate ?? Date()
+            let endDate = workoutSession.endDate ?? Date()
+            let duration = endDate.timeIntervalSince(startDate)
+            let distance = HKQuantity(unit: self.unit ?? HKUnit.mile(), doubleValue: self.distance)
+            
+            let workout = HKWorkout(activityType: self.configuration?.activityType ?? .running,
+                                    start: startDate,
+                                    end: endDate,
+                                    duration: duration,
+                                    totalEnergyBurned: nil,
+                                    totalDistance: distance,
+                                    device: HKDevice.local(),
+                                    metadata: [HKMetadataKeyIndoorWorkout: (self.configuration?.locationType ?? .outdoor) == .indoor])
+            
+            guard healthStore.authorizationStatus(for: HKObjectType.workoutType()) == .sharingAuthorized else {
+                print("*** the app does not have permission to save workout samples ***")
+                return
+            }
+            
+            healthStore.save(workout, withCompletion: { (success, error) -> Void in
+                guard success else {
+                    // Add proper error handling here...
+                    print("*** Could not save the active energy burned samples: \(error?.localizedDescription) ***")
+                    return
+                }
+            })
         }
     }
 }
